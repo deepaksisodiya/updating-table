b2f4089867f70982e7722f84bbabe8423aa18217
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2018-03-30 22:54:13 +0530

    finally added first unit test :)

--- es6/tableView.test.js
+++ es6/tableView.test.js
@@ -1,3 +1,15 @@
-it('checking', () => {
-  expect(1).toBe(1);
-})
+
+var tableRow = require('./TableView');
+jest.mock('./TableView');
+
+beforeEach(() => {
+  // Clear all instances and calls to constructor and all methods:
+  tableRow.mockClear();
+});
+
+describe('TableView', () => {
+  it('Calls Constructor', () => {
+    const TableView = new tableRow();
+    expect(TableView).toBeTruthy();
+  });
+});

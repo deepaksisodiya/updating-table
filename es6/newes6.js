c98b899a80b31aae4a110cab54d8500e96f23644
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2018-03-30 18:53:08 +0530

    render a fake data to the dom

--- es6/newes6.js
+++ es6/newes6.js
@@ -1,11 +1,32 @@
+
+
 class TableView {
   constructor(props) {
-    this.$ = props.id;
+    this.$ = document.getElementById(props.id);
     this.data = props.data;
     this.render();
   }
   render() {
-    console.log('this.data ', this.data)
+    const person = {
+      name: 'Wes',
+      job: 'Web Developer',
+      city: 'Hamilton',
+      bio: 'Wes is a really cool guy that loves to teach web development!'
+    };
+
+    const markup = `
+     <div class="person">
+        <h2>
+            ${person.name}
+        </h2>
+        <p class="location">${person.name}</p>
+        <p class="bio">${person.bio}</p>
+     </div>
+    `;
+
+    this.$.innerHTML = markup;
+
+    console.log('data');
   }
 }
 

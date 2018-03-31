104cdfb35b651f632d9fbfd63dbf03e61a2c8490
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2018-03-31 22:42:54 +0530

    spanshot test

--- es6/utils.test.js
+++ es6/utils.test.js
@@ -15,4 +15,25 @@ describe('updateCurrencyPairsMidPrices', () => {
 
     expect(allCurrencyPairsMidPrices).toMatchSnapshot();
   });
+
+  it('update the array of same company', () => {
+    const data = {
+      "bestBid": 10,
+      "bestAsk": 8,
+      "name": "usdjpy",
+    };
+    const allCurrencyPairsMidPrices = {
+      "usdjpy": [
+        {
+          "midPrice": 3,
+          "time": 1522516237111,
+        },
+      ]
+    };
+    const time = 1522516227135;
+
+    updateCurrencyPairsMidPrices(data, allCurrencyPairsMidPrices, time);
+
+    expect(allCurrencyPairsMidPrices).toMatchSnapshot();
+  });
 });

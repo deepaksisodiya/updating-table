48bcb8229402a9a486900dd97363d1d568a9bd22
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2018-03-31 14:39:59 +0530

    snapshot test for the sorted table by lastChangeBid

--- es6/tableView.test.js
+++ es6/tableView.test.js
@@ -20,4 +20,42 @@ describe('TableView', () => {
 
     expect(div.innerHTML).toMatchSnapshot();
   });
+
+  it('Sorted the given data by lastChangeBid', () => {
+    const div = document.createElement("div");
+    const TableView = new tableRow({
+      node: div,
+      data: {
+        usdjpy: {
+          "name": "usdjpy",
+          "bestBid": 106.7297012204255,
+          "bestAsk": 107.25199883791178,
+          "openBid": 107.22827132623534,
+          "openAsk": 109.78172867376465,
+          "lastChangeAsk": -4.862314256927661,
+          "lastChangeBid": 2
+        },
+        usdeur: {
+          "name": "usdeur",
+          "bestBid": 106.7297012204255,
+          "bestAsk": 107.25199883791178,
+          "openBid": 107.22827132623534,
+          "openAsk": 109.78172867376465,
+          "lastChangeAsk": -4.862314256927661,
+          "lastChangeBid": -10
+        },
+        name: {
+          "name": "euraud",
+          "bestBid": 106.7297012204255,
+          "bestAsk": 107.25199883791178,
+          "openBid": 107.22827132623534,
+          "openAsk": 109.78172867376465,
+          "lastChangeAsk": -4.862314256927661,
+          "lastChangeBid": 1
+        },
+      }
+    });
+
+    expect(div.innerHTML).toMatchSnapshot();
+  });
 });

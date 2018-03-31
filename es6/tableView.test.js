321bcfb6c1efae8e444fe38d3eb12cd5dd66d5e7
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2018-03-31 14:17:59 +0530

    snapshot test for the TableView component

--- es6/tableView.test.js
+++ es6/tableView.test.js
@@ -1,15 +1,23 @@
-
-var tableRow = require('./TableView');
-jest.mock('./TableView');
-
-beforeEach(() => {
-  // Clear all instances and calls to constructor and all methods:
-  tableRow.mockClear();
-});
+const tableRow = require('./TableView');
 
 describe('TableView', () => {
-  it('Calls Constructor', () => {
-    const TableView = new tableRow();
-    expect(TableView).toBeTruthy();
+  it('Call The Constructor and create table', () => {
+    const div = document.createElement("div");
+    const TableView = new tableRow({
+      node: div,
+      data: {
+        gbpaud: {
+          "name": "gbpaud",
+          "bestBid": 1.8899720617188756,
+          "bestAsk": 1.9550019816411313,
+          "openBid": 1.8885006037024021,
+          "openAsk": 1.932899396297598,
+          "lastChangeAsk": 0.06332772101079831,
+          "lastChangeBid": 0.08900450895206324
+        }
+      }
+    });
+
+    expect(div.innerHTML).toMatchSnapshot();
   });
 });
